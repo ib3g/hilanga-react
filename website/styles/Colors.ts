17f75f0b58efea85a3b0134915600b8cdd127de2
ib3g <i.barry3g@gmail.com>
2021-12-04 16:32:09 +0100

    Upgrade Next js to v 12

---
+++ website/styles/Colors.ts
@@ -0,0 +1,28 @@
+const red = '#ff7b9e';
+const blue = '#315ae7';
+const lightBlue = '#6483ee';
+const dark = '#000A20';
+const shadow = '#C8CDE0';
+const green = '#00BE86';
+const white = '#FFFFFF';
+const nuage = '#b7c8f5';
+const lightNuage = '#dfe5f9';
+
+export const palette = {
+  dark,
+  shadow,
+  white,
+  green,
+  red,
+  blue,
+  lightBlue,
+  nuage,
+  lightNuage,
+};
+
+export default {
+  palette,
+  background: palette.lightNuage,
+  primary: palette.dark,
+  disabled: 'rgba(0, 0, 0, 0.3)',
+};
